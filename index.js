b32b522f746a0a7a8ee74e2e4ea9c5bf04527c63
JiangTJ <116749895@qq.com>
2020-02-08 19:17:20 +0800

    Update i18n

--- index.js
+++ index.js
@@ -9,8 +9,23 @@ const cache = new Cache();
 
 utils.loadPlugin('hexo-generator-searchdb');
 
+let defaultLang = {
+  "search.menu": "Search",
+  "search.placeholder": "Searching..."
+}
+let zh = {
+  "search.menu": "搜索",
+  "search.placeholder": "搜索..."
+}
+utils.i18n({
+  "default": defaultLang,
+  "en": defaultLang,
+  "zh": zh,
+  "zh-CN": zh
+})
+
 // Get config
-let defaultConfig = yaml.safeLoad(fs.readFileSync(path.join(__dirname, 'default.yaml')));  
+let defaultConfig = yaml.safeLoad(fs.readFileSync(path.join(__dirname, 'default.yaml')));
 var config = hexo.config.search = Object.assign(defaultConfig, hexo.config.search);
 
 // Generator js
@@ -28,7 +43,7 @@ if (config.script.type === 'dist') {
 injector.register('menu', ctx => {
   return cache.apply('menu', () => {
     let {__, theme} = ctx;
-    let button = (theme.menu_settings.icons?'<i class="menu-item-icon fas fa-search fa-fw"></i>':'') + __('menu.search');
+    let button = (theme.menu_settings.icons ? '<i class="menu-item-icon fas fa-search fa-fw"></i>' : '') + __('search.menu');
     return `<li class="menu-item menu-item-search"><a href="javascript:;" class="popup-trigger">${button}</a></li>`
   })
 })
