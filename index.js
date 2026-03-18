45e1ac9984c43f522b806e662f0539eedb239c74
Mr.J <116749895@qq.com>
2026-03-18 22:15:48 +0800

    style: 统一代码中的引号使用双引号并格式化代码
    将单引号替换为双引号以保持一致性，并调整代码格式使其更规范

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
 /* global hexo */
-'use strict';
+"use strict";
 
-const { join } = require('path');
-const yaml = require('js-yaml');
-const fs = require('fs');
-const ejs = require('ejs');
-const { Cache } = require('hexo-util');
-const utils = require('hexo-cake-utils')(hexo, __dirname);
-const injector = require('hexo-extend-injector2')(hexo);
+const { join } = require("path");
+const yaml = require("js-yaml");
+const fs = require("fs");
+const ejs = require("ejs");
+const { Cache } = require("hexo-util");
+const utils = require("hexo-cake-utils")(hexo, __dirname);
+const injector = require("hexo-extend-injector2")(hexo);
 const cache = new Cache();
 
-const urlFor = hexo.extend.helper.get('url_for').bind(hexo);
+const urlFor = hexo.extend.helper.get("url_for").bind(hexo);
 
 const fa = (ctx, css) => {
   const { fa_inline } = ctx;
   if (!fa_inline) {
     return `<i class='${css}'></i>`;
   }
-  const data = css.split(' ');
-  return fa_inline(data[1].substring(3), {prefix: data[0]});
+  const data = css.split(" ");
+  return fa_inline(data[1].substring(3), { prefix: data[0] });
 };
 
-utils.loadPlugin('hexo-generator-searchdb');
+utils.loadPlugin("hexo-generator-searchdb");
 
 const defaultLang = {
-  'search.menu': 'Search',
-  'search.placeholder': 'Searching...'
+  "search.menu": "Search",
+  "search.placeholder": "Searching...",
 };
 const zh = {
-  'search.menu': '搜索',
-  'search.placeholder': '搜索...'
+  "search.menu": "搜索",
+  "search.placeholder": "搜索...",
 };
 const langs = {
-  'en': defaultLang,
-  'zh': zh,
-  'zh-CN': zh
+  en: defaultLang,
+  zh: zh,
+  "zh-CN": zh,
 };
 const getLangData = (ctx, key) => {
   const { __, page } = ctx;
@@ -46,43 +46,46 @@ const getLangData = (ctx, key) => {
 };
 
 // Get config
-const defaultConfig = yaml.safeLoad(fs.readFileSync(join(__dirname, 'default.yaml')));
+const defaultConfig = yaml.load(
+  fs.readFileSync(join(__dirname, "default.yaml")),
+);
 const config = Object.assign(defaultConfig, hexo.config.search);
 hexo.config.search = config;
 
-injector.register('menu', {
-  value: ctx => {
+injector.register("menu", {
+  value: (ctx) => {
     return cache.apply(`menu-${ctx.page.lang}`, () => {
-      const {theme} = ctx;
+      const { theme } = ctx;
       let icon;
       if (theme.menu_settings && theme.menu_settings.icons === false) {
-        icon = '';
+        icon = "";
       } else {
-        icon = config.menu.icon ? fa(ctx, config.menu.icon) : '';
+        icon = config.menu.icon ? fa(ctx, config.menu.icon) : "";
       }
-      const button = icon + getLangData(ctx, 'search.menu');
+      const button = icon + getLangData(ctx, "search.menu");
       return `<li class="menu-item menu-item-search"><a href="javascript:;" class="popup-trigger">${button}</a></li>`;
     });
   },
-  priority: config.menu.priority
+  priority: config.menu.priority,
 });
 
-
-const template = fs.readFileSync(join(__dirname, 'layout/local-search.ejs')).toString();
-injector.register('body-end', ctx => {
+const template = fs
+  .readFileSync(join(__dirname, "layout/local-search.ejs"))
+  .toString();
+injector.register("body-end", (ctx) => {
   return cache.apply(`script-${ctx.page.lang}`, () => {
-    const placeholder = getLangData(ctx, 'search.placeholder');
+    const placeholder = getLangData(ctx, "search.placeholder");
     return ejs.render(template, {
       placeholder,
-      searchIcon: fa(ctx, 'fas fa-search'),
-      closeIcon: fa(ctx, 'fas fa-times-circle')
+      searchIcon: fa(ctx, "fas fa-search"),
+      closeIcon: fa(ctx, "fas fa-times-circle"),
     });
   });
 });
 
-injector.register('style', join(__dirname, 'layout/local-search.css'));
-injector.register('js', {
+injector.register("style", join(__dirname, "layout/local-search.css"));
+injector.register("js", {
   value: `localsearch = {path: '${urlFor(config.path)}', options: ${JSON.stringify(config.layout)}};`,
-  priority: 1
+  priority: 1,
 });
-injector.register('js', join(__dirname, 'dist/local-search.js'));
+injector.register("js", join(__dirname, "dist/local-search.js"));
